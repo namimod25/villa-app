0fcc216162b0122a51e70156878d3836fe4293a4
dodit2819 <namimod25@gmail.com>
2025-06-06 20:03:15 +0700

    compile komponen

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import {prisma} from '@/lib/prisma';
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
- export const prisma = globalForPrisma.prisma || new PrismaClient()
+ export const prisma = globalForPrisma.prisma ||  PrismaClient
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'development') {
+  
   globalForPrisma.prisma = prisma;
 }
