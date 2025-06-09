b0cd221ccc606a484b532ee217ba7ce99e7feb47
dodit2819 <namimod25@gmail.com>
2025-06-09 23:19:10 +0700

    code

--- types/authjs.d.ts
+++ types/authjs.d.ts
@@ -3,7 +3,7 @@ import {JWT} from 'next-auth/jwt';
 
 declare module "next-auth"{
     interface session {
-        user: User & DefaultSession['user'];
+        user: User & DefaultSession["user"];
     }
     interface User {
         role: string;
